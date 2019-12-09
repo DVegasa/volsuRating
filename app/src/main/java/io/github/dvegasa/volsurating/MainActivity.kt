606b93a82a1b20ad13803ee7285cbf1b0b9a9207
Ed <--->
2019-12-09 20:59:18 +0400

    init commit

---
+++ app/src/main/java/io/github/dvegasa/volsurating/MainActivity.kt
@@ -0,0 +1,12 @@
+package io.github.dvegasa.volsurating
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
