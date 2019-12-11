721c2897f661e2406655adfb2d2323013bb76f4c
Ed <--->
2019-12-11 19:14:37 +0400

    [+] Fundamental RvSubjects
    Took 33 minutes

--- app/src/main/java/io/github/dvegasa/volsurating/screens/main/MainActivity.kt
+++ app/src/main/java/io/github/dvegasa/volsurating/screens/main/MainActivity.kt
@@ -4,13 +4,18 @@ import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
 import io.github.dvegasa.volsurating.R
+import io.github.dvegasa.volsurating.Subject
 import io.github.dvegasa.volsurating.screens.welcome.WelcomeActivity
 import io.github.dvegasa.volsurating.storage.UserDataStorage
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     private val userDataStorage = UserDataStorage(this)
+    private val adapter = RvSubjectsAdapter(arrayListOf())
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,6 +25,29 @@ class MainActivity : AppCompatActivity() {
         } else {
             startActivity(Intent(this, WelcomeActivity::class.java))
             finish()
+            return
         }
+
+        setSupportActionBar(toolbarDef)
+        supportActionBar?.setTitle("ВолГУ рейтинг")
+
+        prepareUI()
+    }
+
+    private fun prepareUI() {
+        adapter.list = arrayListOf(
+            Subject("\uD83D\uDC4D", "Алгебра и теория чисел", 47),
+            Subject("❗", "Математический анализ", 12),
+            Subject("✅", "Геометрия и топология", 100),
+            Subject("\uD83D\uDC4D", "Информатика и програ...", 63),
+            Subject("\uD83D\uDD30", "Русский язык", 65)
+        )
+
+        rvSubjects.adapter = adapter
+        val ll = LinearLayoutManager(this)
+        rvSubjects.layoutManager = ll
+        rvSubjects.addItemDecoration(
+            DividerItemDecoration(rvSubjects.context, ll.orientation)
+        )
     }
 }
---
+++ app/src/main/java/io/github/dvegasa/volsurating/screens/main/RvSubjectsAdapter.kt
@@ -0,0 +1,34 @@
+package io.github.dvegasa.volsurating.screens.main
+
+import android.annotation.SuppressLint
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import io.github.dvegasa.volsurating.R
+import io.github.dvegasa.volsurating.Subject
+import kotlinx.android.synthetic.main.item_subject.view.*
+
+/**
+ * 11.12.2019
+ */
+class RvSubjectsAdapter(var list: ArrayList<Subject>) : RecyclerView.Adapter<RvSubjectsAdapter.VH>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): VH {
+        val v = LayoutInflater.from(parent.context).inflate(R.layout.item_subject, parent, false)
+        return VH(v)
+    }
+
+    override fun getItemCount() = list.size
+
+    override fun onBindViewHolder(holder: VH, position: Int) {
+        holder.bind(position)
+    }
+
+    inner class VH(val v: View) : RecyclerView.ViewHolder(v) {
+        @SuppressLint("SetTextI18n")
+        fun bind(pos: Int) {
+            v.tvSubjectName.setText("${list[pos].emoji} ${list[pos].name}")
+            v.tvRating.setText(list[pos].rating.toString())
+        }
+    }
+}
